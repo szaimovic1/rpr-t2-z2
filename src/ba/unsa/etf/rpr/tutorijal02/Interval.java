204646a68b47ef6de2748144eb21045ddd8177ea
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-10-25 00:17:00 +0200

    pada test equals()

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -7,7 +7,7 @@ public class Interval {
     public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaPripada, boolean krajnjaPripada)
              throws IllegalArgumentException {
         if(pocetnaTacka>krajnjaTacka)
-            throw IllegalArgumentException ("Pocetna tacka veca od krajnje!\n");
+            throw new IllegalArgumentException ("Pocetna tacka veca od krajnje!\n");
         this.pocetnaTacka=pocetnaTacka;
         this.krajnjaTacka=krajnjaTacka;
         this.pocetnaPripada=pocetnaPripada;
@@ -26,34 +26,123 @@ public class Interval {
     }
 
     public boolean isIn(double vrijednostTacke){
-        return (vrijednostTacke>=pocetnaTacka && vrijednostTacke<=krajnjaTacka);
+        return ((vrijednostTacke>pocetnaTacka && vrijednostTacke<krajnjaTacka)
+               || (vrijednostTacke==pocetnaTacka && pocetnaPripada)
+               || (vrijednostTacke==krajnjaTacka && krajnjaPripada));
     }
 
     public Interval intersect(Interval i){
         if(this.equals(i)) return this;
         else if(krajnjaTacka<i.pocetnaTacka || i.krajnjaTacka<pocetnaTacka)
             return new Interval();
-        else if(pocetnaTacka=<i.pocetnaTacka && krajnjaTacka>=i.krajnjaTacka &&
-               )
+        else if(pocetnaTacka<=i.pocetnaTacka && krajnjaTacka>=i.krajnjaTacka ||
+                (pocetnaTacka<=i.pocetnaTacka && krajnjaTacka>i.krajnjaTacka) ||
+                (pocetnaTacka<i.pocetnaTacka && krajnjaTacka>=i.krajnjaTacka) ||
+                (pocetnaTacka==i.pocetnaTacka && krajnjaTacka==i.krajnjaTacka &&
+                 (i.krajnjaPripada || i.pocetnaPripada) && !(i.krajnjaPripada && i.pocetnaPripada)))
             return i;
-        else if(pocetnaTacka>=i.pocetnaTacka && krajnjaTacka=<i.krajnjaTacka &&
-        )
+        else if((pocetnaTacka>i.pocetnaTacka && krajnjaTacka<i.krajnjaTacka) ||
+                (pocetnaTacka>=i.pocetnaTacka && krajnjaTacka<i.krajnjaTacka) ||
+                (pocetnaTacka>i.pocetnaTacka && krajnjaTacka<=i.krajnjaTacka) ||
+                (pocetnaTacka==i.pocetnaTacka && krajnjaTacka==i.krajnjaTacka &&
+                 (krajnjaPripada || pocetnaPripada) && !(krajnjaPripada && pocetnaPripada)))
             return this;
-        else if(){
-
+        else if(pocetnaTacka<i.pocetnaTacka && krajnjaTacka<i.krajnjaTacka){
+            if(krajnjaTacka==i.pocetnaTacka && krajnjaPripada && i.pocetnaPripada)
+                return new Interval(krajnjaTacka, krajnjaTacka, true, true);
+            else if(krajnjaTacka==i.pocetnaTacka && (!krajnjaPripada || !i.pocetnaPripada))
+                return new Interval();
+            else if(i.pocetnaPripada && krajnjaPripada)
+                return new Interval(i.pocetnaTacka, krajnjaTacka, true, true);
+            else if(i.pocetnaPripada)
+                return new Interval(i.pocetnaTacka, krajnjaTacka, true, false);
+            else if(krajnjaPripada)
+                return new Interval(i.pocetnaTacka, krajnjaTacka, false, true);
+            else
+                return new Interval(i.pocetnaTacka, krajnjaTacka, false, false);
         }
-
+        else if(pocetnaTacka>i.pocetnaTacka && krajnjaTacka>i.krajnjaTacka){
+            if(i.krajnjaTacka==pocetnaTacka && i.krajnjaPripada && pocetnaPripada)
+                return new Interval(krajnjaTacka, krajnjaTacka, true, true);
+            else if(i.krajnjaTacka==pocetnaTacka && (!i.krajnjaPripada || !pocetnaPripada))
+                return new Interval();
+            else if(pocetnaPripada && i.krajnjaPripada)
+                return new Interval(i.pocetnaTacka, krajnjaTacka, true, true);
+            else if(pocetnaPripada)
+                return new Interval(i.pocetnaTacka, krajnjaTacka, true, false);
+            else if(i.krajnjaPripada)
+                return new Interval(i.pocetnaTacka, krajnjaTacka, false, true);
+            else
+                return new Interval(i.pocetnaTacka, krajnjaTacka, false, false);
+        }
+        return new Interval();
     }
 
     public static Interval intersect(Interval i1, Interval i2){
-
+        if(i1.equals(i2)) return i1;
+        else if(i1.krajnjaTacka<i2.pocetnaTacka || i2.krajnjaTacka<i1.pocetnaTacka)
+            return new Interval();
+        else if(i1.pocetnaTacka<=i2.pocetnaTacka && i1.krajnjaTacka>=i2.krajnjaTacka ||
+                (i1.pocetnaTacka<=i2.pocetnaTacka && i1.krajnjaTacka>i2.krajnjaTacka) ||
+                (i1.pocetnaTacka<i2.pocetnaTacka && i1.krajnjaTacka>=i2.krajnjaTacka) ||
+                (i1.pocetnaTacka==i2.pocetnaTacka && i1.krajnjaTacka==i2.krajnjaTacka &&
+                        (i2.krajnjaPripada || i2.pocetnaPripada) && !(i2.krajnjaPripada && i2.pocetnaPripada)))
+            return i2;
+        else if((i1.pocetnaTacka>i2.pocetnaTacka && i1.krajnjaTacka<i2.krajnjaTacka) ||
+                (i1.pocetnaTacka>=i2.pocetnaTacka && i1.krajnjaTacka<i2.krajnjaTacka) ||
+                (i1.pocetnaTacka>i2.pocetnaTacka && i1.krajnjaTacka<=i2.krajnjaTacka) ||
+                (i1.pocetnaTacka==i2.pocetnaTacka && i1.krajnjaTacka==i2.krajnjaTacka &&
+                        (i1.krajnjaPripada || i1.pocetnaPripada) && !(i1.krajnjaPripada && i1.pocetnaPripada)))
+            return i1;
+        else if(i1.pocetnaTacka<i2.pocetnaTacka && i1.krajnjaTacka<i2.krajnjaTacka){
+            if(i1.krajnjaTacka==i2.pocetnaTacka && i1.krajnjaPripada && i2.pocetnaPripada)
+                return new Interval(i1.krajnjaTacka, i1.krajnjaTacka, true, true);
+            else if(i1.krajnjaTacka==i2.pocetnaTacka && (!i1.krajnjaPripada || !i2.pocetnaPripada))
+                return new Interval();
+            else if(i2.pocetnaPripada && i1.krajnjaPripada)
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, true, true);
+            else if(i2.pocetnaPripada)
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, true, false);
+            else if(i1.krajnjaPripada)
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, false, true);
+            else
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, false, false);
+        }
+        else if(i1.pocetnaTacka>i2.pocetnaTacka && i1.krajnjaTacka>i2.krajnjaTacka){
+            if(i2.krajnjaTacka==i1.pocetnaTacka && i2.krajnjaPripada && i1.pocetnaPripada)
+                return new Interval(i1.krajnjaTacka, i1.krajnjaTacka, true, true);
+            else if(i2.krajnjaTacka==i1.pocetnaTacka && (!i2.krajnjaPripada || !i1.pocetnaPripada))
+                return new Interval();
+            else if(i1.pocetnaPripada && i2.krajnjaPripada)
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, true, true);
+            else if(i1.pocetnaPripada)
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, true, false);
+            else if(i2.krajnjaPripada)
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, false, true);
+            else
+                return new Interval(i2.pocetnaTacka, i1.krajnjaTacka, false, false);
+        }
+        return new Interval();
     }
 
-    public void toString(){
-
+    public String toString(){
+        String s;
+        if(krajnjaPripada && pocetnaPripada)
+            s = "[" + pocetnaTacka + "," + krajnjaTacka + "]";
+        else if(pocetnaPripada)
+            s = "[" + pocetnaTacka + "," + krajnjaTacka + ")";
+        else if(krajnjaPripada)
+            s = "(" + pocetnaTacka + "," + krajnjaTacka + "]";
+        else if(this.isNull())
+            s = "()";
+        else
+            s = "(" + pocetnaTacka + "," + krajnjaTacka + ")";
+        return s;
     }
 
-    public boolean equals(){
-
+    //@override
+    public boolean equals(Interval i){
+        return ((pocetnaTacka-i.pocetnaTacka)<0.0000001 && (krajnjaTacka-i.krajnjaTacka)<0.0000001
+                && pocetnaPripada==i.pocetnaPripada && krajnjaPripada==i.krajnjaPripada);
     }
 }
--- src/ba/unsa/etf/rpr/tutorijal02/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Main.java
@@ -3,6 +3,13 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nema ništa u mainu, pokrenite testove!");
+        //System.out.println("Nema ništa u mainu, pokrenite testove!");
+
+        Interval i = new Interval(1.1, 2.5, true, false);
+        Interval i2 = new Interval(1.1, 2.5, true, false);
+        if(i.equals(i2))
+            System.out.println("TOOO");
+        System.out.println("TUGY");
+
     }
 }
